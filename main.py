5b507e20cb75dd38a172d3f2700bbf9bc45d9d0e
Jumbeba <jumbeba3@gmail.com>
2021-10-28 19:07:43 -0300

    Criação da mascara para os números

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import re
+
 from TelefonesBr import TelefonesBr
 
 telefone = "551123659874"
 
 telefone_objeto = TelefonesBr(telefone)
 
+# padrao = "([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
+# resposta = re.search(padrao, telefone)
+# print(resposta.group(1))
+
+print(telefone_objeto)
