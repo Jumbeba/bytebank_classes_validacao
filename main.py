ea2c482962ecbf9a2f97f447a9bf70069fe90402
Jumbeba <jumbeba3@gmail.com>
2021-10-28 14:10:00 -0300

    Criando validação de CNPJ

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from CPF_CNPJ import CpfCnpj
+#from validate_docbr import CNPJ
+# cpf_um = Cpf("01234567890")
+# print(cpf_um)
 
-from CPF import Cpf
+exemplo_cnpj = "81994337000263"
+# cnpj = CNPJ()
+# print(cnpj.validate(exemplo_cnpj))
+documento = CpfCnpj(exemplo_cnpj, 'cnpj')
 
-cpf_um = Cpf("01234567890")
-print(cpf_um)
