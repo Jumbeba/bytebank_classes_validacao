10da43a73db7dc7d3e7a6ad0c492ae6e1491159b
Jumbeba <jumbeba3@gmail.com>
2021-10-28 18:38:34 -0300

    Criação da classe e objeto da validação

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
+from TelefonesBr import TelefonesBr
 
-padrao = "\w{5,50}@[a-z]{3,10}.\w{2,3}.\w{2,3}"
-texto = "aaaabbbccc rafael213@gmail.com.br sd65a1d65s1 65d1as 6165 1s6d5sa"
-resposta = re.search(padrao, texto)
-print(resposta.group())
+telefone = "551123659874"
+
+telefone_objeto = TelefonesBr(telefone)
+
