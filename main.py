d0ba1b5e534f0c3b59293443c73ac2090f831935
Jumbeba <jumbeba3@gmail.com>
2021-10-29 12:34:09 -0300

    Criação da validação de data
    usando a biblioteca nativa datetime

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import re
+from datetime import datetime, timedelta
 
-from TelefonesBr import TelefonesBr
+from datas_br import DatasBr
 
-telefone = "551123659874"
-
-telefone_objeto = TelefonesBr(telefone)
-
-# padrao = "([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
-# resposta = re.search(padrao, telefone)
-# print(resposta.group(1))
-
-print(telefone_objeto)
+cadastro = DatasBr()
+print(cadastro.dia_semana())
